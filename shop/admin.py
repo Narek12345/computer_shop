713cecf3a9c365120c2b0b2751db15cbdf5c5d1f
Narek <narekbayanduryan16@gmail.com>
2023-06-18 12:04:41 +0300

    Improved representation of the Computer model in the admin panel.

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+
 from .models import Computer
 
 
-admin.site.register(Computer)
+@admin.register(Computer)
+class ComputerAdmin(admin.ModelAdmin):
+	
+	# Отображаем нужные атрибуты в admin panel.
+	list_display = ('name', 'category')
+
+	# Создаем нужные фильтры в admin panel.
+	list_filter = ('price',)
+
+	# Задаем поля, по которым будет идти поиск в случае запросов.
+	search_fields = ('name',)
+
+	def get_form(self, request, obj=None, **kwargs):
+		form = super().get_form(request, obj, **kwargs)
+		form.base_fields['name'].label = 'Name (only computer!):'
+		return form
